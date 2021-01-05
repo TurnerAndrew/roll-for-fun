079fe82e6ca5c13479c8fa6e8a727bfedebd812b
Andrew Turner <turner.andrew0@gmail.com>
2021-01-04 21:26:08 -0600

    .env

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Header from './Components/UI/Header'
 
 function App() {
   return (
     <div className="App">
-     
+      <Header/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/UI/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>ROLL FOR FUN</h1>
+            <div>Sign In</div>
+            <div>Sign Up</div>
+        </header>
+    )
+}
+
+export default Header
