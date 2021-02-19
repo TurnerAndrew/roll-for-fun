a1bbeb83dc39e584171520849019cb640bcb750e
Andrew Turner <turner.andrew0@gmail.com>
2021-02-19 08:53:27 -0600

    form fields updated

--- src/Components/Auth/Signin.js
+++ src/Components/Auth/Signin.js
@@ -1,65 +1,62 @@
-import {Link} from 'react-router-dom'
-import React, {useState} from 'react'
-import axios from 'axios'
-import {connect} from 'react-redux'
-import {getUserData} from '../../redux/userReducer'
+import { Link } from "react-router-dom"
+import React, { useState } from "react"
+import axios from "axios"
+import { connect } from "react-redux"
+import { getUserData } from "../../redux/userReducer"
 
 const Signin = (props) => {
-    
-    
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
 
-    const login = (e) => {
-        e.preventDefault()
-       
-        axios.post('/auth/signin', {username, password}).then((res) => {
-            props.getUserData(res.data)
-            props.history.push('/home')
+  const login = (e) => {
+    e.preventDefault()
 
-        }).catch(err => console.log(err))
-        }
+    axios
+      .post("/auth/signin", { username, password })
+      .then((res) => {
+        props.getUserData(res.data)
+        props.history.push("/home")
+      })
+      .catch((err) => console.log(err))
+  }
 
-    return (        
-        <div id='signin-page'>
-            <section id='signin-container'>
-                <h1>SIGN IN</h1>
-                <form id='signin' name='signin' method='post' data-netlify='true' netflify-honeypot='bot-field'>
-                <input type='hidden' name='form-name' value='signin'></input>
-                <input
-                    type='text'
-                    name='username'
-                    placeholder='USERNAME'
-                    className='form-input'
-                    onChange={(e) => setUsername(e.target.value)}></input>
-                <input
-                    type='password'
-                    name='password'
-                    placeholder='PASSWORD'
-                    className='form-input'
-                    onChange={(e) => setPassword(e.target.value)}
-                ></input>
-                <button
-                    type='button'
-                    id='signin-button'
-                    onClick={login}>
-                        SIGN IN
-                    </button>
-                </form>
-                <div>
-                    <p>Don't have an account?  Register <Link to='/register'>here!</Link></p>
-                </div>
-            </section>
+  return (
+    <div id='signin-page'>
+      <section id='signin-container'>
+        <h1>SIGN IN</h1>
+        <form id='signin'>
+          <input
+            type='text'
+            name='username'
+            placeholder='USERNAME'
+            className='form-input'
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+          <input
+            type='password'
+            name='password'
+            placeholder='PASSWORD'
+            className='form-input'
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button type='button' id='signin-button' onClick={login}>
+            SIGN IN
+          </button>
+        </form>
+        <div>
+          <p>
+            Don't have an account? Register <Link to='/register'>here!</Link>
+          </p>
         </div>
-    )
-
+      </section>
+    </div>
+  )
 }
 
-function mapStateToProps(state){
-    return {
-        user: state
-    }
-    
+function mapStateToProps(state) {
+  return {
+    user: state,
+  }
 }
 
-export default connect(mapStateToProps, {getUserData})(Signin)
+export default connect(mapStateToProps, { getUserData })(Signin)
